4aadda4dc6e3c3206609f87255d8738db9f9bb6a
joda9 <66819219+joda9@users.noreply.github.com>
2023-07-07 09:15:29 +0200

    website crawler in wind_data_processing eingebaut
    Zur Vermeidung vieler Dateien habe ich die crawler Funktion direkt in die wind data processing datei integriert

--- wind_data_processing.py
+++ wind_data_processing.py
@@ -3,6 +3,8 @@ import numpy as np
 from scipy.optimize import curve_fit
 from tqdm import tqdm
 import locale
+import requests
+from bs4 import BeautifulSoup
 
 
 def preprocess_power_curves(input_file, output_file):
@@ -145,3 +147,135 @@ def process_data(data_wind_path, data_power_curve_path, data_tech_path, save_pat
 
     data_wind = pd.concat([data_wind, result_df], axis=1) # Zusammenführen der verarbeiteten Winddaten mit den Ergebnissen der Leistungskurve
     return data_wind # Rückgabe der verarbeiteten Winddaten
+
+
+
+
+def read_technical_information(url):
+    """
+    Liest die technischen Informationen von einer Webseite aus.
+
+    Args:
+        url (str): URL der Webseite.
+
+    Returns:
+        DataFrame: DataFrame mit den technischen Informationen.
+    """
+    # HTTP-Anfrage senden und den HTML-Inhalt erhalten
+    response = requests.get(url)  # Senden einer HTTP-Anfrage an die angegebene URL
+    html_content = response.content  # Abrufen des Inhalts der Antwort
+
+    # BeautifulSoup verwenden, um den HTML-Inhalt zu analysieren
+    soup = BeautifulSoup(html_content, "html.parser")  # Erstellen eines BeautifulSoup-Objekts zum Analysieren des HTML-Inhalts
+
+    # Alle TabContent finden
+    tab_contents = soup.find_all("div", {"class": "TabContent"})  # Finden aller Elemente mit dem Tag "div" und der Klasse "TabContent"
+
+    if tab_contents:
+        # Leeres DataFrame erstellen
+        df = pd.DataFrame()  # Erstellen eines leeren Pandas DataFrame
+
+        # Durch die Tabellen iterieren
+        for tab_content in tab_contents:
+            # Datenzeilen extrahieren
+            rows = tab_content.find_all("div", {"class": "row"})  # Finden aller Elemente mit dem Tag "div" und der Klasse "row"
+
+            # Leeres DataFrame erstellen
+            data = []
+
+            # Durch die Datenzeilen iterieren und Werte extrahieren
+            for row in rows:
+                left_col = row.find("div", {"class": "col-left"}).text.strip()  # Extrahieren des Texts aus dem Elements mit der Klasse "col-left"
+                right_col = row.find("div", {"class": "col-right"}).text.strip()  # Extrahieren des Texts aus dem Elements mit der Klasse "col-right"
+                data.append([left_col, right_col])  # Hinzufügen der Werte zur Datenliste
+
+            # DataFrame erstellen
+            temp_df = pd.DataFrame(data, columns=['Property', "Value"]).set_index(['Property'])  # Erstellen eines temporären DataFrames mit den extrahierten Werten
+            # Das aktuelle DataFrame an das Gesamt-DataFrame anhängen
+            df = pd.concat([df, temp_df])  # Zusammenführen des temporären DataFrames mit dem Gesamt-DataFrame
+
+        # Doppelte Spalten entfernen
+        df1 = df.drop_duplicates(keep='first')  # Entfernen von doppelten Spalten aus dem DataFrame, wobei die erste Instanz beibehalten wird
+        # Nur die erste Instanz der doppelten Spalte behalten
+        df1 = df1[~df1.index.duplicated(keep='first')]  # Entfernen von doppelten Zeilen aus dem DataFrame, wobei die erste Instanz beibehalten wird
+        df1 = df1.set_index(df1.index).transpose()  # Transponieren des DataFrames und Setzen der Indexspalte als Spaltennamen
+        return df1  # Rückgabe des vorverarbeiteten DataFrames
+    else:
+        print("Keine TabContents gefunden.")  # Ausgabe einer Fehlermeldung, wenn keine Tabelleninhalte gefunden wurden
+
+
+def get_website_title(url):
+    """
+    Holt den Titel einer Webseite.
+
+    Args:
+        url (str): URL der Webseite.
+
+    Returns:
+        str: Titel der Webseite.
+    """
+    # HTTP-Anfrage senden und den HTML-Inhalt erhalten
+    response = requests.get(url)
+    html_content = response.content
+
+    # BeautifulSoup verwenden, um den HTML-Inhalt zu analysieren
+    soup = BeautifulSoup(html_content, "html.parser")
+
+    # Den Titel der Website extrahieren
+    title = soup.title.string
+    return title
+
+
+def add_website_titles(df):
+    """
+    Fügt den Titel der Webseite sowie zu jeder Turbine die technischen Informationen zu einem DataFrame hinzu.
+
+    Args:
+        df (DataFrame): DataFrame mit den Webseiten-Links.
+
+    Returns:
+        DataFrame: DataFrame mit dem hinzugefügten Titel und technischen Infos.
+    """
+    # Neue Spalte "Turbine" zum DataFrame hinzufügen
+    df["Turbine"] = ""  # Hinzufügen einer leeren Spalte mit dem Namen "Turbine" zum DataFrame
+
+    # Alle Links im DataFrame durchgehen
+    df_technical_infos = pd.read_excel('data/technical_information.xlsx')  # Einlesen der technischen Informationen aus einer Excel-Datei
+
+    if not ('check' in df_technical_infos.columns):  # Überprüfen, ob die Spalte "check" im DataFrame vorhanden ist
+        df_technical_infos['check'] = np.nan  # Hinzufügen einer neuen Spalte "check" mit NaN-Werten zum DataFrame
+
+    counter = 0  # Initialisieren des Zählers
+    progress_bar = tqdm(total=len(df), desc="Progress")  # Erstellen einer Fortschrittsanzeige
+
+    while (counter / (len(df) - 1) < 1):  # Schleife, die solange läuft, bis alle Links verarbeitet wurden
+        counter = 0  # Zurücksetzen des Zählers
+
+        for index, row in df_technical_infos.iterrows():  # Iteration über jede Zeile im DataFrame
+            if np.isnan(df_technical_infos['check'][index]):  # Überprüfen, ob die "check"-Spalte für den aktuellen Link NaN ist
+                try:
+                    link = row["Links"]  # Extrahieren des Links aus der aktuellen Zeile
+
+                    # Den Titel der Website abrufen
+                    title = get_website_title(link)  # Aufrufen der Funktion "get_website_title", um den Titel der Webseite zu erhalten
+
+                    # Technische Informationen abrufen
+                    tech_infos = read_technical_information(link)  # Aufrufen der Funktion "read_technical_information", um die technischen Informationen der Webseite zu erhalten
+
+                    for column in tech_infos:  # Iteration über jede Spalte in den technischen Informationen
+                        df_technical_infos.at[index, column] = tech_infos[column]['Value']  # Aktualisieren der entsprechenden Zelle im DataFrame mit dem Wert aus den technischen Informationen
+
+                    df_technical_infos.at[index, 'check'] = 1  # Aktualisieren der "check"-Spalte auf 1, um anzuzeigen, dass der Link verarbeitet wurde
+
+                    # Den Titel in die entsprechende Zeile des DataFrames einfügen
+                    df_technical_infos.at[index, "Turbine"] = title  # Hinzufügen des Titels in die "Turbine"-Spalte des DataFrames
+
+                    df_technical_infos.to_excel('data/technical_information.xlsx')  # Speichern des aktualisierten DataFrames in einer Excel-Datei
+                    progress_bar.update(1)  # Aktualisieren der Fortschrittsanzeige um 1
+                except Exception as e:
+                    print(e)  # Ausgabe einer Fehlermeldung, falls ein Fehler auftritt
+            else:
+                progress_bar.update(1)  # Aktualisieren der Fortschrittsanzeige um 1
+                counter += 1  # Inkrementieren des Zählers
+
+    return df_technical_infos  # Rückgabe des DataFrames mit den hinzugefügten Titeln
